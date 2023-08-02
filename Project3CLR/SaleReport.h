15c0309c4768e995bd58a8845c324665a9c444d8
amallaltlai <amal.lal@outlook.com>
2023-08-02 21:27:19 +0530

    added sale report windows form

--- Project3CLR/SaleReport.h
+++ Project3CLR/SaleReport.h
@@ -34,7 +34,12 @@ namespace Project3CLR {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
+
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::DataGridView^ currentSaleGridView;
 	protected:
 
 	private:
@@ -50,43 +55,103 @@ namespace Project3CLR {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle1 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle2 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle3 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->currentSaleGridView = (gcnew System::Windows::Forms::DataGridView());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->currentSaleGridView))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// tableLayoutPanel1
+			// button1
 			// 
-			this->tableLayoutPanel1->ColumnCount = 7;
-			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
-				40.44715F)));
-			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
-				59.55285F)));
-			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
-				237)));
-			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
-				230)));
-			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
-				210)));
-			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
-				177)));
-			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
-				160)));
-			this->tableLayoutPanel1->Location = System::Drawing::Point(26, 22);
-			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
-			this->tableLayoutPanel1->RowCount = 2;
-			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 106)));
-			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 8)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(1421, 580);
-			this->tableLayoutPanel1->TabIndex = 0;
-			this->tableLayoutPanel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &SaleReport::tableLayoutPanel1_Paint);
+			this->button1->Location = System::Drawing::Point(26, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(189, 33);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"New Sale";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(221, 12);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(189, 33);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"Store";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(416, 12);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(189, 33);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"New Order";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(1643, 22);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(87, 34);
+			this->button4->TabIndex = 4;
+			this->button4->Text = L"Refresh";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// currentSaleGridView
+			// 
+			dataGridViewCellStyle1->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
+			dataGridViewCellStyle1->BackColor = System::Drawing::SystemColors::Control;
+			dataGridViewCellStyle1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			dataGridViewCellStyle1->ForeColor = System::Drawing::SystemColors::WindowText;
+			dataGridViewCellStyle1->SelectionBackColor = System::Drawing::SystemColors::Highlight;
+			dataGridViewCellStyle1->SelectionForeColor = System::Drawing::SystemColors::HighlightText;
+			dataGridViewCellStyle1->WrapMode = System::Windows::Forms::DataGridViewTriState::True;
+			this->currentSaleGridView->ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			this->currentSaleGridView->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			dataGridViewCellStyle2->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
+			dataGridViewCellStyle2->BackColor = System::Drawing::SystemColors::Window;
+			dataGridViewCellStyle2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			dataGridViewCellStyle2->ForeColor = System::Drawing::SystemColors::ControlText;
+			dataGridViewCellStyle2->SelectionBackColor = System::Drawing::SystemColors::Highlight;
+			dataGridViewCellStyle2->SelectionForeColor = System::Drawing::SystemColors::HighlightText;
+			dataGridViewCellStyle2->WrapMode = System::Windows::Forms::DataGridViewTriState::False;
+			this->currentSaleGridView->DefaultCellStyle = dataGridViewCellStyle2;
+			this->currentSaleGridView->Location = System::Drawing::Point(26, 64);
+			this->currentSaleGridView->Name = L"currentSaleGridView";
+			dataGridViewCellStyle3->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
+			dataGridViewCellStyle3->BackColor = System::Drawing::SystemColors::Control;
+			dataGridViewCellStyle3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			dataGridViewCellStyle3->ForeColor = System::Drawing::SystemColors::WindowText;
+			dataGridViewCellStyle3->SelectionBackColor = System::Drawing::SystemColors::Highlight;
+			dataGridViewCellStyle3->SelectionForeColor = System::Drawing::SystemColors::HighlightText;
+			dataGridViewCellStyle3->WrapMode = System::Windows::Forms::DataGridViewTriState::True;
+			this->currentSaleGridView->RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+			this->currentSaleGridView->RowHeadersWidth = 62;
+			this->currentSaleGridView->RowTemplate->Height = 28;
+			this->currentSaleGridView->Size = System::Drawing::Size(1714, 553);
+			this->currentSaleGridView->TabIndex = 5;
 			// 
 			// SaleReport
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1735, 676);
-			this->Controls->Add(this->tableLayoutPanel1);
+			this->ClientSize = System::Drawing::Size(1773, 676);
+			this->Controls->Add(this->currentSaleGridView);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
 			this->Name = L"SaleReport";
 			this->Text = L"SaleReport";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->currentSaleGridView))->EndInit();
 			this->ResumeLayout(false);
 
 		}
